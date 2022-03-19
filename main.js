a22eb83b04f8951e18cf14f480d97d27e87a62aa
gutoo01 <gu.trindadelive@gmail.com>
2022-03-19 10:46:43 -0300

    .fix https

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
  */
 
 const getData = () => {
-  fetch(`http://api.github.com/users/gutoo01`).then(response => {
+  fetch(`https://api.github.com/users/gutoo01`).then(response => {
     return response.json()
   }).then(body => {
     console.log(body);
